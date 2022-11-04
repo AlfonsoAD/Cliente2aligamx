1f8e8de480fc9122a20fc711b74067bdbae355ce
AlfonsoAD <alfonsoad097@gmail.com>
2022-11-03 20:34:07 -0500

    cambios en login

--- j2aligamx/pages/session/login.js
+++ j2aligamx/pages/session/login.js
@@ -1,5 +1,5 @@
-import LayoutSession from "../components/Session/LayoutSession";
-import FormLog from "../components/Session/FormLog";
+import LayoutSession from "../../components/Session/LayoutSession";
+import FormLog from "../../components/Session/FormLog";
 
 export default function Home() {
   return (
