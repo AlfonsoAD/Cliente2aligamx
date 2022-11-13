e24f1d0a0337beb37ab143b91e9e1255b9bc95ed
Cesar-CCS <CesarCastro_S@outlook.com>
2022-11-12 22:27:21 -0600

    tabla

---
+++ j2aligamx/components/Main/TableResults.js
@@ -0,0 +1,187 @@
+const TableResults = () => {
+    const logoAmerica =
+    "https://user-images.githubusercontent.com/90345024/201499006-ad675a3a-dfd7-44bb-8092-a06563131922.png";
+  const logoCruzAzul =
+    "https://user-images.githubusercontent.com/90345024/201499029-8de7b773-6efa-4289-9c47-79619bc93fd4.png";
+  const logoPachuca =
+    "https://user-images.githubusercontent.com/90345024/201499147-7ba088f4-c63d-4283-88de-62bd75ca2c37.png";
+  const logoMonterrey =
+    "https://user-images.githubusercontent.com/90345024/201499200-01f8dc09-6c9b-46e6-99ef-c4d2ea911d13.jpg";
+  return (
+    <div className="flex m-4 shadow-2xl ... rounded-lg">
+      <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div className="py-2 inline-block min-w-full sm:px-6 lg:px-8">
+          <div className="overflow-hidden">
+            <table className="min-w-full">
+              <thead className="bg-white border-b">
+                <tr>
+                  <th
+                    scope="col"
+                    className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
+                  >
+                    Pos
+                  </th>
+                  <th
+                    scope="col"
+                    className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
+                  ></th>
+                  <th
+                    scope="col"
+                    className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
+                  >
+                    PTS
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr className="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
+                  <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                    1
+                  </td>
+                  <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                    <img src={logoAmerica} width="20" height="20" />
+                  </td>
+                  <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                    35
+                  </td>
+                </tr>
+                <tr className="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
+                  <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                    1
+                  </td>
+                  <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                    <img src={logoAmerica} width="20" height="20" />
+                  </td>
+                  <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                    35
+                  </td>
+                </tr>
+                <tr className="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
+                  <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                    1
+                  </td>
+                  <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                    <img src={logoAmerica} width="20" height="20" />
+                  </td>
+                  <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                    35
+                  </td>
+                </tr>
+                <tr className="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
+                  <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                    1
+                  </td>
+                  <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                    <img src={logoAmerica} width="20" height="20" />
+                  </td>
+                  <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                    35
+                  </td>
+                </tr>
+                <tr className="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
+                  <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                    1
+                  </td>
+                  <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                    <img src={logoAmerica} width="20" height="20" />
+                  </td>
+                  <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                    35
+                  </td>
+                </tr>
+                <tr className="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
+                  <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                    1
+                  </td>
+                  <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                    <img src={logoAmerica} width="20" height="20" />
+                  </td>
+                  <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                    35
+                  </td>
+                </tr>
+                <tr className="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
+                  <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                    1
+                  </td>
+                  <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                    <img src={logoAmerica} width="20" height="20" />
+                  </td>
+                  <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                    35
+                  </td>
+                </tr>
+                <tr className="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
+                  <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                    1
+                  </td>
+                  <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                    <img src={logoAmerica} width="20" height="20" />
+                  </td>
+                  <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                    35
+                  </td>
+                </tr>
+                <tr className="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
+                  <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                    1
+                  </td>
+                  <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                    <img src={logoAmerica} width="20" height="20" />
+                  </td>
+                  <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                    35
+                  </td>
+                </tr>
+                <tr className="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
+                  <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                    1
+                  </td>
+                  <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                    <img src={logoAmerica} width="20" height="20" />
+                  </td>
+                  <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                    35
+                  </td>
+                </tr>
+                <tr className="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
+                  <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                    1
+                  </td>
+                  <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                    <img src={logoAmerica} width="20" height="20" />
+                  </td>
+                  <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                    35
+                  </td>
+                </tr>
+                <tr className="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
+                  <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                    1
+                  </td>
+                  <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                    <img src={logoAmerica} width="20" height="20" />
+                  </td>
+                  <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                    35
+                  </td>
+                </tr>
+                <tr className="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
+                  <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                    1
+                  </td>
+                  <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                    <img src={logoAmerica} width="20" height="20" />
+                  </td>
+                  <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                    35
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
