6614b5b82e5e4ac804534d35e1dc483a7b69ce98
Cesar-CCS <CesarCastro_S@outlook.com>
2022-11-13 13:47:52 -0600

    partidos 0.1

--- j2aligamx/components/Main/SmallContainerBox.js
+++ j2aligamx/components/Main/SmallContainerBox.js
@@ -1,11 +1,8 @@
 const SmallContainerBox = ({ children }) => {
   return (
     <div className="shadow-2xl ... m-6 w-80 h-80 bg-white">
-      <h1>Jornada 1</h1>
-      <p>01/07      21:05</p>
       {children}
-      <p>2 - 4</p>
-      </div>
+    </div>
   );
 };
 
--- j2aligamx/pages/partidos.js
+++ j2aligamx/pages/partidos.js
@@ -7,14 +7,31 @@ import TableResults from "../components/Main/TableResults";
 export default function Home() {
   return (
     <LayoutMain>
-      <div className="flex justify-centerflex-wrap">
+      <div className="flex justify-center flex-wrap">
         <div className="m-4 w-4/5">
           <SelectJornada />
           <MatchContainer>
+            <div className="grid justify-items-stretch">
             <SmallContainerBox className="m-4 w-1/3">hola</SmallContainerBox>
             <SmallContainerBox className="m-4 w-1/3">hola</SmallContainerBox>
-            <SmallContainerBox className="m-4 w-1/3">hola</SmallContainerBox>
-            <SmallContainerBox>hola2</SmallContainerBox>
+            <SmallContainerBox>
+            <h1>Jornada 1</h1>
+            <p>01/07      21:05</p>
+            <div className="justify-items-stretch">
+              <img 
+              src="https://user-images.githubusercontent.com/90345024/201499006-ad675a3a-dfd7-44bb-8092-a06563131922.png"
+              width="20" 
+              height="20"/>
+              <p className="m-4 w-1/3">VS</p>
+              <img 
+              src="https://user-images.githubusercontent.com/90345024/201499006-ad675a3a-dfd7-44bb-8092-a06563131922.png"
+              width="20" 
+              height="20"/>
+            </div>
+            <p>2 - 4</p>
+            </SmallContainerBox>
+            </div>
+            <SmallContainerBox>hola</SmallContainerBox>
             <SmallContainerBox>hola3</SmallContainerBox>
             <SmallContainerBox>hola</SmallContainerBox>
             <SmallContainerBox>hola2</SmallContainerBox>
