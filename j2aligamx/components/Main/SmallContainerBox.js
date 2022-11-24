aa31c7d601a5caff9f6beef6ed80221e1f540d8d
AlfonsoAD <alfonsoad097@gmail.com>
2022-11-24 12:23:08 -0600

    cambios en small

--- j2aligamx/components/Main/SmallContainerBox.js
+++ j2aligamx/components/Main/SmallContainerBox.js
@@ -1,16 +1,8 @@
-<<<<<<< HEAD
 const SmallContainerBox = ({ children, id }) => {
   return (
     <div key={id} className="shadow-2xl ... m-6 w-80 h-80 bg-white">
       {children}
     </div>
-=======
-const SmallContainerBox = ({children}) => {
-  return (
-    <div className="shadow-2xl ... m-6 w-80 h-80 bg-white">
-      {children}
-      </div>
->>>>>>> 40de37046118e1f034c16f5bcef90367c2bd1c1f
   );
 };
 
