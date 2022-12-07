e6b3550b7a0678c7c065bfba282cfc799517a47b
AlfonsoAD <alfonsoad097@gmail.com>
2022-12-07 12:19:45 -0600

    cambios layoutmain

--- j2aligamx/pages/_app.js
+++ j2aligamx/pages/_app.js
@@ -1,6 +1,4 @@
 //BY JESÚS ALFONSO ANDRADE DOMÍNGUEZ 18100149
-//react, next
-import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 //Context
 import UserPreferencesProvider from "../components/Context/UserPreferencesProvider";
@@ -9,23 +7,6 @@ import UserProvider from "../components/Context/UserProvider";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  const router = useRouter();
-
-  useEffect(() => {
-    validando();
-  }, []);
-
-  const validando = () => {
-    if (
-      localStorage.getItem("accessToken") === null &&
-      localStorage.getItem("refreshToken") === null
-    ) {
-      setTimeout(() => {
-        router.push("/session/login");
-      }, 2000);
-    }
-  };
-
   return (
     <UserProvider>
       <UserPreferencesProvider>
